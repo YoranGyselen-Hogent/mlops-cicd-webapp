6b14366dd63d5dc9c5b372f1ffce34312994cbf2
Yoran Gyselen <yoran.gyselen@student.hogent.be>
2024-10-21 11:58:06 +0200

    Fixed linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,63 +1,52 @@
 export default [
-    {
-        "env": {
-            "es6": true,
-            "es2021": true,
-            "node": true,
-            "mocha": true
+  {
+    languageOptions: {
+      ecmaVersion: 2021, // Set the ECMAScript version
+      globals: {
+        global: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        require: 'readonly',
+        // Mocha globals
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
+    rules: {
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
         },
-        "extends": "eslint:recommended",
-        "parserOptions": {
-            "ecmaVersion": 12,
-            "sourceType": "module"
+      ],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-tabs': 'error',
+      'max-len': [
+        'error',
+        {
+          code: 120,
+          tabWidth: 2,
         },
-        "rules": {
-            "indent": [
-                "error",
-                2,
-                {
-                    "SwitchCase": 1
-                }
-            ],
-            "linebreak-style": [
-                "error",
-                "unix"
-            ],
-            "quotes": [
-                "error",
-                "single"
-            ],
-            "semi": [
-                "error",
-                "always"
-            ],
-            "comma-dangle": [
-                "error",
-                "always-multiline"
-            ],
-            "no-tabs": [
-                "error"
-            ],
-            "max-len": [
-                "error",
-                {
-                    "code": 120,
-                    "tabWidth": 2
-                }
-            ],
-            "arrow-parens": [
-                "error",
-                "always"
-            ],
-            "brace-style": [
-                "error",
-                "1tbs",
-                {
-                    "allowSingleLine": false
-                }
-            ],
-            "no-inner-declarations": "off"
+      ],
+      'arrow-parens': ['error', 'always'],
+      'brace-style': [
+        'error',
+        '1tbs',
+        {
+          allowSingleLine: false,
         },
-        "ignores": ['node_modules/**'], // Specify files or directories to ignore
-    }
-]
+      ],
+      'no-inner-declarations': 'off',
+    },
+  },
+];
